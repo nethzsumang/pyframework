50506ffc8889b19c29c29abc64d18f74302268a2
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-01-30 16:48:10 +0800

    Removed development code.

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
 from bootstrap import bootstrap
 from framework.Utilities.Packager import *
-from framework.Utilities.Misc.Log import Log
-Log.debug('Hello in debug!')
-Log.error('Hello in error!')
-Log.info('Hello in info!')
 
 
-# version_check()
-# o_app = bootstrap.start()
+version_check()
+o_app = bootstrap.start()
