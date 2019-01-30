a8ae4a8d37db90bc15964d706b5c374579714b21
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-01-30 15:26:52 +0800

    WIP: Encryption, Cache

--- framework/Utilities/Cache/Cache.py
+++ framework/Utilities/Cache/Cache.py
@@ -4,3 +4,11 @@ import pickle
 class Cache:
     def __init__(self):
         pass
+
+    @staticmethod
+    def add(s_name, m_data):
+        pass
+
+    @staticmethod
+    def remove(s_name):
+        pass
