0b5ee65fabf0b4a867165ab53595dd23e0fd253a
Kenneth Sumang <kennethsumang08@gmail.com>
2019-02-03 21:31:01 +0800

    Internationalization module finished.

--- app/controllers/IndexController.py
+++ app/controllers/IndexController.py
@@ -6,7 +6,4 @@ class IndexController(Controller):
 	@staticmethod
 	def index(o_app, a_params):
 		print('This is ' + o_app.get('APP', 'APP_NAME') + ' app.')
-		o_app.dump(o_app.get_group('LANG'))
-		o_app.set_lang('en')
-		o_app.dump(o_app.get_group('LANG'))
 		return Controller.redirect(False)
