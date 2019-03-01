c2688582565a333acce5297209ceacbf8d2fe50f
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-03-01 14:08:48 +0800

    Added select operation for MySQL client.

--- framework/Data/Database/Clients/MySQLClient.py
+++ framework/Data/Database/Clients/MySQLClient.py
@@ -1,5 +1,28 @@
 from framework.Data.Database.Clients.BaseClient import BaseClient
+import MySQLdb 
+
+from urllib import parse
 
 
-class MySQLCLient(BaseClient):
-    pass
+class MySQLClient(BaseClient):
+    def connect(self):
+        self.conn = MySQLdb.connect(
+            host=self.options["options"]["host"],
+            port=self.options["options"]["port"],
+            user=self.options["options"]["user"],
+            passwd=self.options["options"]["password"],
+            db=self.options["options"]["database"],
+        )
+        self.cursor = self.conn.cursor()
+    
+    def select(self, table, cols=[]):
+        col_names = ''
+        if len(cols) > 0:
+            cols = map(lambda colname: parse.quote(colname), cols)
+            col_names = ','.join(cols)
+        else:
+            col_names = '*'
+        
+        table = parse.quote(table)
+        self.cursor.execute('SELECT ' + col_names + ' FROM ' + table)
+        return self.cursor.fetchall()
--- framework/Data/Database/Database.py
+++ framework/Data/Database/Database.py
@@ -1,11 +1,15 @@
 from framework.Data.File.JSONFile import JSONFile
 from framework.Utilities.Misc.Utils import path_join
+from framework.Data.Database.Clients.Client import Client
 
 
 class Database:
     def __init__(self):
         pass
     
-    @staticmethod
-    def init():
+    def init(self):
         a_options = JSONFile(path_join('config', 'database.json'), 'r').read()
+        self.client = Client.connect(a_options)
+
+    def select(self, table, cols=[]):
+        return self.client.select(table, cols)
