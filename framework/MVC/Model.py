eeca3cd6d936ba9ca0aa96473eaa1ad47d0704d0
Kenneth Sumang <kennethsumang08@gmail.com>
2019-03-03 19:53:43 +0800

    Added Database's Select support on Model.

--- framework/MVC/Model.py
+++ framework/MVC/Model.py
@@ -6,3 +6,6 @@ class Model(abc.ABC):
     def __init__(self):
         if self.db_ref:
             self.data_source = Database().init()
+
+    def select(self, cols=[]):
+        return self.data_source.select(self.table, cols)
