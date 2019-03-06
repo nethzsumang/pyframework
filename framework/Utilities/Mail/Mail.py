b82c6ef4bc27d74e57673f27949e892e0e699621
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-03-06 13:54:34 +0800

    Mail constructor change. Added validator for valid email.

--- framework/Utilities/Mail/Mail.py
+++ framework/Utilities/Mail/Mail.py
@@ -2,12 +2,13 @@ import smtplib
 
 
 class Mail:
-    def __init__(self, options):
-        self.options = {
-            "host": options["host"],
-            "port": options["port"],
-            "default_sender": options["sender"],
-        }
+    def __init__(self):
+        from framework.Data.File.JSONFile import JSONFile
+        import os
+        from pathlib import Path
+
+        lang_data_path = str(Path.cwd()) + os.sep + "config" + os.sep + "mail.json"
+        self.options = JSONFile(lang_data_path, "r").read()
 
     def send(self, message, to, sender=None):
         sender = sender if sender is not None else self.options["default_sender"]
@@ -18,3 +19,15 @@ class Mail:
             return True
         except smtplib.SMTPException:
             return False
+
+    @staticmethod
+    def is_valid_email(email):
+        import re
+
+        if len(email) > 7:
+            return bool(
+                re.match(
+                    "^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email
+                )
+            )
+        return False
