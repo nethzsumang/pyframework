a33e97a4f0b1ed2e9949fcdaf8114fbcf3ce5949
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-03-14 15:03:42 +0800

    DocBlock (1)

--- framework/Data/Database/Clients/Client.py
+++ framework/Data/Database/Clients/Client.py
@@ -4,6 +4,11 @@ from framework.Data.Database.Clients.MySQLClient import MySQLClient
 class Client:
     @staticmethod
     def connect(options):
+        """
+            Connects to the client.
+            :param options:
+            :return:
+        """
         if options["client"] == "mysql":
             client = MySQLClient(options)
             client.connect()
