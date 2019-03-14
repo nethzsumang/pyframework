7b0c9d7581bdc1693f4642722f170cf000aa571b
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-03-14 14:57:13 +0800

    DocBlocks.

--- framework/Charts/matplotlib/LineGraph.py
+++ framework/Charts/matplotlib/LineGraph.py
@@ -2,13 +2,39 @@ import matplotlib.pyplot as plt
 
 
 class LineGraph:
+    """
+        (list) X Values for Chart
+    """
     a_x_values = None
+
+    """
+        (list) Y Values for Chart
+    """
     a_y_values = None
+
+    """
+        (string) Title for the Chart
+    """
     s_title = ""
+
+    """
+        (string) Label for X axis
+    """
     s_x_label = ""
+
+    """
+        (string) Label for Y axis
+    """
     s_y_label = ""
+
+    """
+        Counter for graph
+    """
     counter = 1
 
+    """
+        (dict) Properties for chart
+    """
     props = {
         "linestyle": "--",
         "colors": ["b", "g", "r", "c", "m", "y"],
@@ -22,9 +48,18 @@ class LineGraph:
     }
 
     def __init__(self, title=""):
+        """
+            LineGraph constructor
+            :param title:
+        """
         self.s_title = title
 
     def set_props(self, a_props):
+        """
+            Sets properties
+            :param a_props:
+            :return:
+        """
         for s_name, s_val in a_props.items():
             if s_name not in self.props:
                 continue
@@ -32,12 +67,26 @@ class LineGraph:
             self.props[s_name] = s_val
 
     def set_values(self, a_x_values, a_y_values, s_x_label="", s_y_label=""):
+        """
+            Set chart's values
+            :param a_x_values:
+            :param a_y_values:
+            :param s_x_label:
+            :param s_y_label:
+            :return:
+        """
         self.a_x_values = a_x_values
         self.a_y_values = a_y_values
         self.s_x_label = s_x_label
         self.s_y_label = s_y_label
 
     def plot(self, a_legend=None, s_title="Untitled"):
+        """
+            Plots the value in the charts
+            :param a_legend:
+            :param s_title:
+            :return:
+        """
         i_counter = 0
         plt.subplot(1, 2, self.counter)
         self.counter += 1
@@ -65,5 +114,9 @@ class LineGraph:
             )
 
     def show(self):
+        """
+            Show the chart
+            :return:
+        """
         self.counter = 0
         plt.show()
