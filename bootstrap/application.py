a0aad50d83c216fc25bd782b96dc4a534d2039bc
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-04-11 11:14:42 +0800

    Transferred a class to another file.

---
+++ bootstrap/application.py
@@ -0,0 +1,78 @@
+class Application:
+    setting_key = "default"
+
+    def __init__(self, data):
+        """
+        AppConstant's constructor
+        
+        Arguments:
+            data {dict} -- Config JSON files data
+        """
+
+        self.data = data
+
+    def get(self, group, key):
+        """
+        Gets a key in the config dictionary.
+        
+        Arguments:
+            group {str} -- Config group (filename in CAPS)
+            key   {str} -- Config key
+        
+        Returns:
+            string -- The value of that key in config.
+        """
+
+        return self.data[group][key]
+
+    def get_group(self, group):
+        """
+        Gets the config data of a whole config file.
+        
+        Arguments:
+            group {str} -- Config group (filename in CAPS)
+        
+        Returns:
+            dict -- Config data of that file.
+        """
+
+        return self.data[group]
+
+    def dump(self, var):
+        """
+        Dumps a variable in the console.
+        
+        Arguments:
+            m_var {mixed} -- Variable to be printed.
+        """
+
+        from var_dump import var_dump
+
+        var_dump(var)
+
+    def get_lang(self):
+        """
+        Gets the current language setting.
+        
+        Returns:
+            str -- Language code.
+        """
+
+        return self.data["LANG"]["current"]
+
+    def set_lang(self, lang):
+        """
+        Sets the current language setting.
+        
+        Arguments:
+            lang {str} -- Language code to set.
+        """
+
+        from framework.Data.File.JSONFile import JSONFile
+        from pathlib import Path
+
+        lang_data_path = path_join(str(Path.cwd()), "config", "lang.json")
+        lang_data = JSONFile(lang_data_path, "r").read()
+        lang_data["current"] = lang
+        JSONFile(lang_data_path, "w").write(lang_data)
+        self.data["LANG"] = JSONFile(lang_data_path, "r").read()
