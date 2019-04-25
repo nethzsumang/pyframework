55f1976348258935d5c53bfd71a59a0635d17fe9
Kenneth Sumang <kenneth02@simplexi.com.ph>
2019-04-25 09:22:46 +0800

    Removed function and imports that is not used.

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -1,5 +1,3 @@
-from framework.Data.File.JSONFile import JSONFile
-from framework.Utilities.Misc.Utils import path_join, filename_split
 from bootstrap.application import Application
 
 
@@ -26,48 +24,6 @@ def start():
     router.begin_router()
     show_exit()
     exit(0)
-
-
-def execute_controller(
-    app, s_name="IndexController", s_method="index", params=None
-):
-    """
-    Executes a function in the controller given.
-    
-    Arguments:
-        app {object} -- AppConstant's object.
-    
-    Keyword Arguments:
-        s_name {str} -- The controller to be executed. (default: {"IndexController"})
-        s_method {str} -- The method to be executed. (default: {"index"})
-        params {[type]} -- Parameters to pass to the controller method. (default: {None})
-    
-    Returns:
-        mixed -- The return value of the controller method that was executed.
-    """
-
-    import glob
-    import os
-
-    a_file_list = glob.glob("app" + os.sep + "controllers" + os.sep + "*.py")
-    a_file_list = [os.path.abspath(path) for path in a_file_list]
-    a_controllers = []
-    for a_file in a_file_list:
-        _, filename = os.path.split(a_file)
-        filename = filename[:-3]
-        a_controllers.append(filename)
-
-    if s_name not in a_controllers:
-        print(s_name + " does not exist!")
-        exit(1)
-
-    if params is None:
-        params = {}
-
-    from pydoc import locate
-
-    _class = locate("app.controllers." + s_name + "." + s_name)
-    return getattr(_class, s_method)(app, params)
 
 
 def show_title(app):
